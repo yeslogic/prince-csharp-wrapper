a349078e2a844bf2ce1e05a88fbc1814a4db9113
Adrian Wong <adrianwjw@gmail.com>
2021-09-28 16:46:46 +1000

    [netstandard2.0] Specify types during object creation
    Target-typed object creation is a C# 9.0 feature.

--- PrinceXML.Wrapper/Enums.cs
+++ PrinceXML.Wrapper/Enums.cs
@@ -7,27 +7,27 @@ namespace PrinceXML.Wrapper.Enums
 {
     public class AuthMethod : Enumeration
     {
-        public static AuthMethod Basic = new(0, "basic");
-        public static AuthMethod Digest = new(1, "digest");
-        public static AuthMethod Ntlm = new(2, "ntlm");
-        public static AuthMethod Negotiate = new(3, "negotiate");
+        public static AuthMethod Basic = new AuthMethod(0, "basic");
+        public static AuthMethod Digest = new AuthMethod(1, "digest");
+        public static AuthMethod Ntlm = new AuthMethod(2, "ntlm");
+        public static AuthMethod Negotiate = new AuthMethod(3, "negotiate");
 
         private AuthMethod(int id, string name) : base(id, name) {}
     }
 
     public class AuthScheme : Enumeration
     {
-        public static AuthScheme Http = new(0, "http");
-        public static AuthScheme Https = new(1, "https");
+        public static AuthScheme Http = new AuthScheme(0, "http");
+        public static AuthScheme Https = new AuthScheme(1, "https");
 
         private AuthScheme(int id, string name) : base(id, name) {}
     }
 
     public class InputType : Enumeration
     {
-        public static InputType Auto = new(0, "auto");
-        public static InputType Html = new(1, "html");
-        public static InputType Xml = new(2, "xml");
+        public static InputType Auto = new InputType(0, "auto");
+        public static InputType Html = new InputType(1, "html");
+        public static InputType Xml = new InputType(2, "xml");
 
         private InputType(int id, string name) : base(id, name) {}
     }
@@ -40,58 +40,58 @@ namespace PrinceXML.Wrapper.Enums
 
     public class PdfProfile : Enumeration
     {
-        public static PdfProfile PdfA_1A = new(0, "PDF/A-1a");
-        public static PdfProfile PdfA_1A_And_PdfUA_1 = new(1, "PDF/A-1a+PDF/UA-1");
-        public static PdfProfile PdfA_1B = new(2, "PDF/A-1b");
-        public static PdfProfile PdfA_2A = new(3, "PDF/A-2a");
-        public static PdfProfile PdfA_2A_And_PdfUA_1 = new(4, "PDF/A-2a+PDF/UA-1");
-        public static PdfProfile PdfA_2B = new(5, "PDF/A-2b");
-        public static PdfProfile PdfA_3A = new(6, "PDF/A-3a");
-        public static PdfProfile PdfA_3A_And_PdfUA_1 = new(7, "PDF/A-3a+PDF/UA-1");
-        public static PdfProfile PdfA_3B = new(8, "PDF/A-3b");
-        public static PdfProfile PdfUA_1 = new(9, "PDF/UA-1");
-        public static PdfProfile PdfX_1A_2001 = new(10, "PDF/X-1a:2001");
-        public static PdfProfile PdfX_1A_2003 = new(11, "PDF/X-1a:2003");
-        public static PdfProfile PdfX_3_2002 = new(12, "PDF/X-3:2002");
-        public static PdfProfile PdfX_3_2003 = new(13, "PDF/X-3:2003");
-        public static PdfProfile PdfX_4 = new(14, "PDF/X-4");
+        public static PdfProfile PdfA_1A = new PdfProfile(0, "PDF/A-1a");
+        public static PdfProfile PdfA_1A_And_PdfUA_1 = new PdfProfile(1, "PDF/A-1a+PDF/UA-1");
+        public static PdfProfile PdfA_1B = new PdfProfile(2, "PDF/A-1b");
+        public static PdfProfile PdfA_2A = new PdfProfile(3, "PDF/A-2a");
+        public static PdfProfile PdfA_2A_And_PdfUA_1 = new PdfProfile(4, "PDF/A-2a+PDF/UA-1");
+        public static PdfProfile PdfA_2B = new PdfProfile(5, "PDF/A-2b");
+        public static PdfProfile PdfA_3A = new PdfProfile(6, "PDF/A-3a");
+        public static PdfProfile PdfA_3A_And_PdfUA_1 = new PdfProfile(7, "PDF/A-3a+PDF/UA-1");
+        public static PdfProfile PdfA_3B = new PdfProfile(8, "PDF/A-3b");
+        public static PdfProfile PdfUA_1 = new PdfProfile(9, "PDF/UA-1");
+        public static PdfProfile PdfX_1A_2001 = new PdfProfile(10, "PDF/X-1a:2001");
+        public static PdfProfile PdfX_1A_2003 = new PdfProfile(11, "PDF/X-1a:2003");
+        public static PdfProfile PdfX_3_2002 = new PdfProfile(12, "PDF/X-3:2002");
+        public static PdfProfile PdfX_3_2003 = new PdfProfile(13, "PDF/X-3:2003");
+        public static PdfProfile PdfX_4 = new PdfProfile(14, "PDF/X-4");
 
         private PdfProfile(int id, string name) : base(id, name) {}
     }
 
     public class RasterBackground : Enumeration
     {
-        public static RasterBackground White = new(0, "white");
-        public static RasterBackground Transparent = new(1, "transparent");
+        public static RasterBackground White = new RasterBackground(0, "white");
+        public static RasterBackground Transparent = new RasterBackground(1, "transparent");
 
         private RasterBackground(int id, string name) : base(id, name) {}
     }
 
     public class RasterFormat : Enumeration
     {
-        public static RasterFormat Auto = new(0, "auto");
-        public static RasterFormat Png = new(1, "png");
-        public static RasterFormat Jpeg = new(2, "jpeg");
+        public static RasterFormat Auto = new RasterFormat(0, "auto");
+        public static RasterFormat Png = new RasterFormat(1, "png");
+        public static RasterFormat Jpeg = new RasterFormat(2, "jpeg");
 
         private RasterFormat(int id, string name) : base(id, name) {}
     }
 
     public class SslType : Enumeration
     {
-        public static SslType Pem = new(0, "PEM");
-        public static SslType Der = new(1, "DER");
+        public static SslType Pem = new SslType(0, "PEM");
+        public static SslType Der = new SslType(1, "DER");
 
         private SslType(int id, string name) : base(id, name) {}
     }
 
     public class SslVersion : Enumeration
     {
-        public static SslVersion Default = new(0, "default");
-        public static SslVersion TlsV1 = new(1, "tlsv1");
-        public static SslVersion TlsV1_0 = new(2, "tlsv1.0");
-        public static SslVersion TlsV1_1 = new(3, "tlsv1.1");
-        public static SslVersion TlsV1_2 = new(4, "tlsv1.2");
-        public static SslVersion TlsV1_3 = new(5, "tlsv1.3");
+        public static SslVersion Default = new SslVersion(0, "default");
+        public static SslVersion TlsV1 = new SslVersion(1, "tlsv1");
+        public static SslVersion TlsV1_0 = new SslVersion(2, "tlsv1.0");
+        public static SslVersion TlsV1_1 = new SslVersion(3, "tlsv1.1");
+        public static SslVersion TlsV1_2 = new SslVersion(4, "tlsv1.2");
+        public static SslVersion TlsV1_3 = new SslVersion(5, "tlsv1.3");
 
         private SslVersion(int id, string name) : base(id, name) {}
     }
